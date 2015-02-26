2ba20e103a5f8a06b510d7d56777cfe13084d23d
brownjame <brown_jame@bentley.edu>
2015-02-26 10:19:24 -0500

    Additional Site Files
    Original Files that Huang typed up

---
+++ js/javaScriptHome.js
@@ -0,0 +1,22 @@
+﻿var BLACK = false;
+var AVATAR = "images/book256";
+//AVATAR = "distortedgumbyimages/gumby"+fix(fnr(0,300),3);
+//AVATAR = "images/avatar";
+window.onload = function () {
+// updated by jd robertson: Fri, Jan 9, 2015
+	var html = "";
+	//html += "<div id='_avatar64'><img src='"+AVATAR+".png' class='_siz64'></div>";
+	html += "<div id='_name'>";
+	html += "<div id='_firstname'>TEAM VULCAN</div>";
+	html += "<div id='_lastname'>[CS460]</div>";
+	html += "<div id='_headersubtext'>COMING SOON</div>";
+	
+	html += "<div id='_headerlogo'><img src='http://s15.postimg.org/4bnr3thon/Bentley_University_Logo.jpg'/> </div>";
+
+	//html += "<div id='_shortname'>JDRobertson</div>";
+	html += "</div>";
+	//html += "<div id='_headback'><img src='"+AVATAR+".png' class='_siz196'></div>";
+	document.getElementsByTagName("header")[0].innerHTML = html; // set header
+	html = "";
+	//html += "<span id='_avatar32'><img src='"+AVATAR+".png' class='_siz32'></span>";
+	}
