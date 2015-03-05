d4502d082ff58ed27deab27a128897e1fc2a7c01
dschof41 <schofer_dani@bentley.edu>
2015-03-05 19:29:08 +0000

    Yelp API practice
    working on the yelp file to be able to pull specific pieces of data

--- yelp.php
+++ yelp.php
@@ -6,13 +6,13 @@
  * by using the Search API to query for businesses by a search term and location,
  * and the Business API to query additional information about the top result
  * from the search query.
- * 
+ *
  * Please refer to http://www.yelp.com/developers/documentation for the API documentation.
- * 
+ *
  * This program requires a PHP OAuth2 library, which is included in this branch and can be
  * found here:
  *      http://oauth.googlecode.com/svn/code/php/
- * 
+ *
  * Sample usage of the program:
  * `php sample.php --term="bars" --location="San Francisco, CA"`
  */
@@ -20,7 +20,7 @@
 // Enter the path that the oauth library is in relation to the php file
 require_once('php/OAuth.php');
 
-// Set your OAuth credentials here  
+// Set your OAuth credentials here
 // These credentials can be obtained from the 'Manage API Access' page in the
 // developers documentation (http://www.yelp.com/developers)
 $CONSUMER_KEY = 'kxzX4eDwht8jG3OsEtY5qw';
@@ -37,12 +37,12 @@ $SEARCH_PATH = '/v2/search/';
 $BUSINESS_PATH = '/v2/business/';
 
 
-/** 
+/**
  * Makes a request to the Yelp API and returns the response
- * 
- * @param    $host    The domain host of the API 
+ *
+ * @param    $host    The domain host of the API
  * @param    $path    The path of the APi after the domain
- * @return   The JSON response from the request      
+ * @return   The JSON response from the request
  */
 function request($host, $path) {
     $unsigned_url = "http://" . $host . $path;
@@ -57,88 +57,103 @@ function request($host, $path) {
     $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
 
     $oauthrequest = OAuthRequest::from_consumer_and_token(
-        $consumer, 
-        $token, 
-        'GET', 
+        $consumer,
+        $token,
+        'GET',
         $unsigned_url
     );
-    
+
     // Sign the request
     $oauthrequest->sign_request($signature_method, $consumer, $token);
-    
+
     // Get the signed URL
     $signed_url = $oauthrequest->to_url();
-    
+
     // Send Yelp API Call
     $ch = curl_init($signed_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $data = curl_exec($ch);
     curl_close($ch);
-    
+
     return $data;
 }
 
 /**
- * Query the Search API by a search term and location 
- * 
- * @param    $term        The search term passed to the API 
- * @param    $location    The search location passed to the API 
- * @return   The JSON response from the request 
+ * Query the Search API by a search term and location
+ *
+ * @param    $term        The search term passed to the API
+ * @param    $location    The search location passed to the API
+ * @return   The JSON response from the request
  */
 function search($term, $location) {
     $url_params = array();
-    
+
     $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
     $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
     $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
     $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
-    
+
     return request($GLOBALS['API_HOST'], $search_path);
 }
 
 /**
  * Query the Business API by business_id
- * 
+ *
  * @param    $business_id    The ID of the business to query
- * @return   The JSON response from the request 
+ * @return   The JSON response from the request
  */
 function get_business($business_id) {
     $business_path = $GLOBALS['BUSINESS_PATH'] . $business_id;
-    
+
     return request($GLOBALS['API_HOST'], $business_path);
 }
 
 /**
- * Queries the API by the input values from the user 
- * 
+ * Queries the API by the input values from the user
+ *
  * @param    $term        The search term to query
  * @param    $location    The location of the business to query
  */
-function query_api($term, $location) {     
+function query_api($term, $location) {
+	print "Query set to Bars in Waltham, only looks for 3 results and only pulls data for 1st result<br><br>";
     $response = json_decode(search($term, $location));
     $business_id = $response->businesses[0]->id;
-    
+
+   	$bus = $response->businesses[0];
+    $phone = $bus->phone;
+    $location = $bus->location->display_address;
+    $street = $location[0];
+    $state = $location[1];
+
+
     print sprintf(
-        "%d businesses found, querying business info for the top result \"%s\"<br>",         
+        "%d businesses found, querying business info for the top result \"%s\"<br>",
         count($response->businesses),
         $business_id
     );
-    
+
     $response = get_business($business_id);
-    
-    print sprintf("Result for business \"%s\" <br> found:<br>", $business_id);
+
+    print sprintf("Result for business \"%s\" <br><br> All data found:<br>", $business_id);
     print "$response<br>";
+
+    print "<br><br>Practice pulling specific pieces of data:<br>$phone<br>$street<br>$state";
+
+
+
+
+
 }
 
 /**
- * User input is handled here 
+ * User input is handled here
  */
 $longopts  = array(
     "term::Bars",
     "location::Boston",
 );
-    
+
 $options = getopt("", $longopts);
 
 $term = $options['term'] ?: '';
