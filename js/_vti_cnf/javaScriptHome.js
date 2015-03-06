2c3addadf4674c713a730263a2dd320638fe0b10
HLuBentleyU <huanglu1031@outlook.com>
2015-03-06 17:53:49 -0500

    Added a signup button on homepage
    Added a clickable signup form button in the header on homepage

--- js/_vti_cnf/javaScriptHome.js
+++ js/_vti_cnf/javaScriptHome.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|06 Mar 2015 20:33:26 -0000
+vti_timelastmodified:TR|06 Mar 2015 22:52:45 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|Vulcan/index.html
 vti_author:SR|BLUE\\LU_HUAN
 vti_modifiedby:SR|BLUE\\LU_HUAN
 vti_timecreated:TR|25 Feb 2015 02:58:50 -0000
-vti_nexttolasttimemodified:TW|06 Mar 2015 20:33:26 -0000
-vti_cacheddtm:TX|25 Feb 2015 02:58:50 -0000
+vti_nexttolasttimemodified:TW|06 Mar 2015 22:52:42 -0000
+vti_cacheddtm:TX|06 Mar 2015 22:52:42 -0000
 vti_filesize:IR|1074
